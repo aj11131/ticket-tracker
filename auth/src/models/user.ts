0a746532e126eec083d9b97aac94facdeea16561
root <root@DESKTOP-V8K1P71.localdomain>
2022-07-26 08:59:50 -0500

    Implement auth service

---
+++ auth/src/models/user.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import { Password } from "../services/password";
+
+// An interface that describes the properties that are required to create a new user
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+const userSchema = new mongoose.Schema<UserAttrs>(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+  done();
+});
+
+const User = mongoose.model("User", userSchema);
+
+export { User };
